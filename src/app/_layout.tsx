e5f5b436ff235be94fb787094b202fd71718d179
seungdeok <akk9132@naver.com>
2025-01-01 17:04:58 +0900

    feat: add SafeAreaView hoc

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { SplashScreen, Tabs } from "expo-router";
 import { useSystemFont } from "@moeum/common/hooks/useSystemFont";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -7,9 +8,11 @@ export default function RootLayout() {
   useSystemFont();
 
   return (
-    <Tabs initialRouteName="index" screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "home" }} />
-      <Tabs.Screen name="setting" options={{ title: "setting" }} />
-    </Tabs>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tabs initialRouteName="index" screenOptions={{ headerShown: false }}>
+        <Tabs.Screen name="index" options={{ title: "home" }} />
+        <Tabs.Screen name="setting" options={{ title: "setting" }} />
+      </Tabs>
+    </SafeAreaView>
   );
 }
